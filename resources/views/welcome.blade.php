3e3afd98b394e446b9bf993949a0aba56fe90be2
nycofox <github@alkis.net>
2023-10-10 08:58:41 +0200

    Fixed bug in routes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,12 +12,13 @@
             <p class="mt-2">Where:</p>
 
             <ul>
-                <li><code>{size}</code> - The desired size of the avatar in pixels (e.g., 300).</li>
+                <li><code>{size}</code> - The desired size of the avatar in pixels (e.g., 300). Maximum size is 500.</li>
                 <li><code>{sex}</code> - The sex of the avatar, which can be 'm' for male, 'f' for female, or blank for
                     random
                 </li>
                 <li><code>{identifier}</code> - A unique string that consistently maps to the same avatar. If not
-                    provided, a random avatar will be returned
+                    provided, a random avatar will be returned. If provided, the same avatar will always be returned,
+                    however it will still vary based on the chosen sex. So a hash can have up to two different avatars.
                 </li>
             </ul>
 
